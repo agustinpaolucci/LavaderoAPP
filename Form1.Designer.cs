c30154809a80d81c27043c66069a5a9b43b4302d
agustinpaolucci <111289424+agustinpaolucci@users.noreply.github.com>
2023-11-24 13:57:28 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace LavaderoAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProductos = new System.Windows.Forms.Label();
+            this.cboProductos = new System.Windows.Forms.ComboBox();
+            this.btnProx = new System.Windows.Forms.Button();
+            this.btnUlt = new System.Windows.Forms.Button();
+            this.lblProximo = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblProductos
+            // 
+            this.lblProductos.AutoSize = true;
+            this.lblProductos.Location = new System.Drawing.Point(80, 33);
+            this.lblProductos.Name = "lblProductos";
+            this.lblProductos.Size = new System.Drawing.Size(110, 13);
+            this.lblProductos.TabIndex = 0;
+            this.lblProductos.Text = "Listado de Productos:";
+            // 
+            // cboProductos
+            // 
+            this.cboProductos.FormattingEnabled = true;
+            this.cboProductos.Location = new System.Drawing.Point(83, 64);
+            this.cboProductos.Name = "cboProductos";
+            this.cboProductos.Size = new System.Drawing.Size(292, 21);
+            this.cboProductos.TabIndex = 2;
+            // 
+            // btnProx
+            // 
+            this.btnProx.Location = new System.Drawing.Point(83, 110);
+            this.btnProx.Name = "btnProx";
+            this.btnProx.Size = new System.Drawing.Size(75, 23);
+            this.btnProx.TabIndex = 3;
+            this.btnProx.Text = "Próximo ID";
+            this.btnProx.UseVisualStyleBackColor = true;
+            this.btnProx.Click += new System.EventHandler(this.btnProx_Click);
+            // 
+            // btnUlt
+            // 
+            this.btnUlt.Location = new System.Drawing.Point(300, 110);
+            this.btnUlt.Name = "btnUlt";
+            this.btnUlt.Size = new System.Drawing.Size(75, 23);
+            this.btnUlt.TabIndex = 4;
+            this.btnUlt.Text = "Último ID";
+            this.btnUlt.UseVisualStyleBackColor = true;
+            this.btnUlt.Click += new System.EventHandler(this.btnUlt_Click);
+            // 
+            // lblProximo
+            // 
+            this.lblProximo.AutoSize = true;
+            this.lblProximo.Location = new System.Drawing.Point(80, 167);
+            this.lblProximo.Name = "lblProximo";
+            this.lblProximo.Size = new System.Drawing.Size(47, 13);
+            this.lblProximo.TabIndex = 5;
+            this.lblProximo.Text = "Proximo:";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(300, 167);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(444, 211);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lblProximo);
+            this.Controls.Add(this.btnUlt);
+            this.Controls.Add(this.btnProx);
+            this.Controls.Add(this.cboProductos);
+            this.Controls.Add(this.lblProductos);
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Listador de Articulos";
+            this.TransparencyKey = System.Drawing.Color.Orange;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProductos;
+        private System.Windows.Forms.ComboBox cboProductos;
+        private System.Windows.Forms.Button btnProx;
+        private System.Windows.Forms.Button btnUlt;
+        private System.Windows.Forms.Label lblProximo;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Reflection.Emit;
+
+
+/*
+ CADENA DE CONEXION LOCAL:
+"Data Source=.\SQLEXPRESS;Initial Catalog=LAVADERO;Integrated Security=True;"
+ */
+
+namespace LavaderoAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarComboProductos();
+            cboProductos.SelectedIndex = -1;
+            lblProximo.Text += proximoNumero();
+        }
+
+        private string proximoNumero()
+        {
+            SqlConnection cnn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=LAVADERO;Integrated Security=True;");
+            SqlCommand comando = new SqlCommand();
+            cnn.Open();
+            comando.Connection = cnn;
+            comando.CommandText = "SP_PROXIMO_ID";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parametro = new SqlParameter();
+            parametro.ParameterName = "@next";
+            parametro.DbType = DbType.Int32;
+            parametro.Direction = ParameterDirection.Output;
+
+            comando.Parameters.Add(parametro);
+            comando.ExecuteNonQuery();
+
+            string proximoCodigo = parametro.Value.ToString();
+
+            cnn.Close();
+
+            return proximoCodigo;
+        }
+
+
+        private void CargarComboProductos()
+        {
+            SqlConnection cnn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=LAVADERO;Integrated Security=True;");
+            cnn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandText = "SP_COMBO_PRODUCTOS";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            DataTable tablaCombo = new DataTable();
+            tablaCombo.Load(cmd.ExecuteReader());
+            cboProductos.DataSource = tablaCombo;
+            cboProductos.ValueMember = "id_producto";
+            cboProductos.DisplayMember = "nombre_producto";
+            cboProductos.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboProductos.SelectedIndex = -1;
+
+            cnn.Close();
+        }
+
+
+        private void btnProx_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=LAVADERO;Integrated Security=True;");
+            SqlCommand cmd = new SqlCommand();
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "SP_PROXIMO_ID";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = "@next";
+            param.DbType = DbType.Int32;
+            param.Direction = ParameterDirection.Output;
+
+            cmd.Parameters.Add(param);
+            cmd.ExecuteNonQuery();
+
+            int proximoCodigo = Convert.ToInt32(param.Value);
+
+            cnn.Close();
+
+            MessageBox.Show("PROXIMO CODIGO: " + proximoCodigo, "INFORMACION");
+        }
+
+        private void btnUlt_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog = LAVADERO; Integrated Security = True;");
+            SqlCommand cmd = new SqlCommand();
+
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "SP_ULTIMO_ID";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = "@last";
+            param.SqlDbType = SqlDbType.Int;
+            param.Direction = ParameterDirection.Output;
+
+            cmd.Parameters.Add(param);
+            cmd.ExecuteNonQuery();
+
+            int ultimoCodigo;
+
+            ultimoCodigo = (int)param.Value;
+            
+            cnn.Close();
+
+            MessageBox.Show("ULTIMO CODIGO CARGADO: " +ultimoCodigo, "INFORMACION");
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("¿Desea salir?","SALIR",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            
+        }
+    }
+}
